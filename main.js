4b9b2602f061fdcda2b02b9dc6a870cfe073cd9b
Dongwan Seo <NoahLuculent@gmail.com>
2024-08-07 17:54:10 +0900

    로그인 페이지 폰트랑 아이콘만 남음

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
+//처음 화면에서 로그인 화면으로 전환
 document.addEventListener("click", function () {
   const page1 = document.getElementById("page1");
   const page2 = document.getElementById("page2");
 
-  // Fade out current page
   page1.classList.add("hidden");
 
-  // After fade-out complete, fade-in new page
   setTimeout(function () {
     page1.style.display = "none";
     page2.style.display = "flex";
     page2.classList.remove("hidden");
-  }, 500); // Matches the CSS transition duration
+  }, 500); // css 전환 시간이랑 맞추기
+});
+
+// 로그인 form
+let loginFrom = document.querySelector(".my-form");
+
+loginFrom.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+
+  console.log("Email: ", email.value);
+  console.log("Password: ", password.value);
+
+  //이후에 API로 전송하는 부분이 필요함
 });
